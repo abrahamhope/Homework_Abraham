99e30a08d3722ed35e81f45b51e3ab39cd6b3692
abrahamhope <abrahamisaachope@gmail.com>
2020-03-24 15:31:20 -0500

    update

--- src/test/java/com/automation/Homework3/TestCasesAll.java
+++ src/test/java/com/automation/Homework3/TestCasesAll.java
@@ -12,7 +12,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class RegistrationForm {
+public class TestCasesAll {
     private WebDriver driver;
     private String URL = "https://practice-cybertekschool.herokuapp.com/registration_form";
 
@@ -20,9 +20,6 @@ public class RegistrationForm {
     public void setup(){
         WebDriverManager.chromedriver().version("79").setup();
         driver = new ChromeDriver();
-        driver.get(URL);
-        driver.manage().window().maximize();
-        BrowserUtils.wait(3);
     }
 
     /*
@@ -33,6 +30,9 @@ public class RegistrationForm {
      */
     @Test
     public void test1(){
+        driver.get(URL);
+        driver.manage().window().maximize();
+
         WebElement birthday =driver.findElement(By.name("birthday"));
         birthday.sendKeys("wrong_dob");
         WebElement invalidBtdyMessage=driver.findElement(By.xpath("//*[@id=\"registrationForm\"]/div[8]/div/small[2]"));
@@ -47,6 +47,8 @@ public class RegistrationForm {
      */
     @Test
     public void test2(){
+        driver.get(URL);
+        driver.manage().window().maximize();
         WebElement cPlusPlus=driver.findElement(By.xpath("//*[@id=\"registrationForm\"]/div[11]/div/div[1]/label"));
         WebElement java = driver.findElement(By.xpath("//*[@id=\"registrationForm\"]/div[11]/div/div[2]/label"));
         WebElement javaScript= driver.findElement(By.xpath("//*[@id=\"registrationForm\"]/div[11]/div/div[3]/label"));
@@ -66,6 +68,8 @@ public class RegistrationForm {
      */
     @Test
     public void test3(){
+        driver.get(URL);
+        driver.manage().window().maximize();
         WebElement firstname =driver.findElement(By.name("firstname"));
         firstname.sendKeys("a");
         WebElement invalidFNameMessage=driver.findElement(By.xpath("//*[@id=\"registrationForm\"]/div[1]/div/small[2]"));
@@ -82,6 +86,8 @@ public class RegistrationForm {
      */
     @Test
     public void test4(){
+        driver.get(URL);
+        driver.manage().window().maximize();
         WebElement lastName =driver.findElement(By.name("lastname"));
         lastName.sendKeys("a");
         WebElement invalidLNameMessage=driver.findElement(By.xpath("//*[@id=\"registrationForm\"]/div[2]/div/small[2]"));
@@ -108,7 +114,8 @@ registration!”
      */
     @Test
     public void test5() {
-
+        driver.get(URL);
+        driver.manage().window().maximize();
         driver.findElement(By.name("firstname")).sendKeys("isaac");
         driver.findElement(By.name("lastname")).sendKeys("hope");
         driver.findElement(By.name("username")).sendKeys("ihoprede");
@@ -132,7 +139,91 @@ registration!”
 
         Assert.assertEquals(actual, expected);
     }
+    @Test
+    public void test6() {
+        //Step 1. Go to "https://www.tempmailaddress.com/"
+        driver.get("https://www.tempmailaddress.com/");
+        driver.manage().window().maximize();
+        BrowserUtils.wait(3);
+        //Step 2. Copy and save email as a string.
+        String emailAddress = driver.findElement(By.id("email")).getText();
 
+        //Step 3. Then go to “https://practice- cybertekschool.herokuapp.com”
+        driver.navigate().to("http://practice.cybertekschool.com/");
+        BrowserUtils.wait(5);
+
+        //Step 4. And click on “Sign Up For Mailing List".
+        WebElement signUpForMailingList = driver.findElement(By.linkText("Sign Up For Mailing List"));
+        signUpForMailingList.click();
+        BrowserUtils.wait(2);
+
+        //Step 5. Enter any valid name.
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mike Johnson");
+
+        //Step 6. Enter email from the Step 2.
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys(emailAddress);
+
+        //Step 7. Click Sign Up
+        WebElement signupBtn = driver.findElement(By.name("wooden_spoon"));
+        signupBtn.click();
+
+        //Step 8. Verify that following message is displayed: “Thank you for signing up.
+        // Click the button below to return to the home page.”
+        WebElement signupMessage = driver.findElement(By.name("signup_message"));
+        Assert.assertTrue(signupMessage.isDisplayed());
+
+        //Step 9. Navigate back to the “https:// www.tempmailaddress.com/”
+        driver.navigate().back();
+        driver.navigate().back();
+        BrowserUtils.wait(5);
+        driver.navigate().back();
+        BrowserUtils.wait(5);
+
+        //Step 10. Verify that you’ve received an email from
+        //“do-not-reply@practice.cybertekschool.com”
+        WebElement emailAd = driver.findElement(By.xpath("//table/tbody/tr[1]/td"));
+        System.out.println("displayed :" + emailAd.isDisplayed());
+        System.out.println("emailAd :" + emailAd.getText());
+        String displayedEmail = emailAd.getText().trim();
+        Assert.assertEquals(displayedEmail, "do-not-reply@practice.cybertekschool.com");
+
+        //Step 11. Click on that email to open it.
+        emailAd.click();
+        BrowserUtils.wait(3);
+
+        //Step 12. Verify that email is from: “do-not- reply@practice.cybertekschool.com”
+        WebElement from= driver.findElement(By.id("odesilatel"));
+        String expectedFrom= "do-not-reply@practice.cybertekschool.com";
+        String actualFrom = from.getText();
+        Assert.assertEquals(actualFrom,expectedFrom);
+
+        //Step 13. Verify that subject is: “Thanks for subscribing to practice.cybertekschool.com!”
+        WebElement subject = driver.findElement(By.id("predmet"));
+        String expectedSubject= "Thanks for subscribing to practice.cybertekschool.com!";
+        String actualSubject=subject.getText();
+        Assert.assertEquals(actualSubject,expectedSubject);
+    }
+
+    @Test
+    public void test7(){
+        driver.get("https://practice-cybertekschool.herokuapp.com");
+        driver.findElement(By.linkText("File Upload")).click();
+        BrowserUtils.wait(2);
+        WebElement choseFile=driver.findElement(By.id("file-upload"));
+        choseFile.sendKeys("/Users/isikdurmus/Desktop/File upload.txt");
+        BrowserUtils.wait(2);
+        driver.findElement(By.id("file-submit")).click();
+        WebElement fileUploadedMessage=driver.findElement(By.xpath("//*[.='File Uploaded!']"));
+        String expected= "File Uploaded!";
+        String actual=fileUploadedMessage.getText();
+        Assert.assertEquals(actual,expected);
+
+        WebElement uploadedFileName = driver.findElement(By.id("uploaded-files"));
+
+        Assert.assertTrue(uploadedFileName.isDisplayed());
+    }
     @AfterMethod
     public void teardown(){
         driver.quit();
