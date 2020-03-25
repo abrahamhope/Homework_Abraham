994110c20bb287f82560f1819e699cc4be9df58c
abrahamhope <abrahamisaachope@gmail.com>
2020-03-25 16:26:09 -0500

    Homework4

--- src/test/java/com/automation/Homework/Homework3.java
+++ src/test/java/com/automation/Homework/Homework3.java
@@ -1,4 +1,4 @@
-package com.automation.Homework3;
+package com.automation.Homework;
 
 import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,7 +12,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class TestCasesAll {
+public class Homework3 {
     private WebDriver driver;
     private String URL = "https://practice-cybertekschool.herokuapp.com/registration_form";
 
--- src/test/java/com/automation/Homework/Homework4.java
+++ src/test/java/com/automation/Homework/Homework4.java
@@ -1,13 +1,10 @@
-package com.automation.Homework3;
+package com.automation.Homework;
 
-import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
